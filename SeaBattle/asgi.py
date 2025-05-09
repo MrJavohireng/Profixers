aada7465e6c1d0fc59de2666de1d6ff154208c3e
Javohir a <2411000757@my.lerna.uz>
2025-05-09 21:14:21 +0500

    Start

--- SeaBattle/asgi.py
+++ SeaBattle/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CaloriaTracker project.
+ASGI config for SeaBattle project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CaloriaTracker.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SeaBattle.settings")
 
 application = get_asgi_application()
