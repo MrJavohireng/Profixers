f6f33292e9948485a595a362a05704f0fd54d122
Javohir a <2411000757@my.lerna.uz>
2025-05-09 21:21:37 +0500

    start

--- ProFixers/asgi.py
+++ ProFixers/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SeaBattle project.
+ASGI config for ProFixers project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SeaBattle.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProFixers.settings")
 
 application = get_asgi_application()
--- ProFixers/urls.py
+++ ProFixers/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for SeaBattle project.
+URL configuration for ProFixers project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
